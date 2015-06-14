6bf69d83a89101e76b640e75a739743e7deb354a
Jeff Way <jway@repx.me>
2015-06-13 20:15:02 -0700

    Add DecisionController where all the real action happens

--- src/AppBundle/Controller/DecisionController.php
+++ src/AppBundle/Controller/DecisionController.php
@@ -2,27 +2,53 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Decision;
 use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class DecisionController extends Controller
+class DecisionController extends SerializerController
 {
     /**
-     * @Route("/decisions.json");
+     * @Route(
+     *   "/decisions.{_format}",
+     *   name="decisions_list",
+     *   defaults={"_format": "html"},
+     *   requirements={
+     *     "_format": "html|json|xml|yml"
+     *   },
+     *   options={"expose"=true})
      * @Security("has_role('ROLE_USER')")
      */
-    public function listAction()
+    public function listAction(Request $request)
     {
-        /** @var User $user */
+        $decisionManager = $this->get('manager.decision');
+
         $user = $this->get('security.token_storage')->getToken()->getUser();
+        $decisions = $decisionManager->getDecisionsForUserWithAnswers($user);
 
-        $json = $this->get('jms_serializer')->serialize($user->getDecisions()->getValues(), 'json');
+        if ($request->getRequestFormat() === 'html') {
+            return $this->render('decisions/list.html.twig', [
+                'decisions' => $decisions
+            ]);
+        } else {
+            return $this->serialize($decisions, $request->getRequestFormat());
+        }
+    }
 
-        return new Response($json, 200, [
-            'Content-Type' => 'application/json'
-        ]);
+    /**
+     * @Route("/decision/{id}/answer.json", name="decision_answer", options={"expose"=true})
+     * @Security("is_granted('manage', decision)")
+     *
+     * @param Decision $decision
+     * @return Response
+     */
+    public function answerAction(Decision $decision)
+    {
+        $choice = $this->get('manager.decision')->pickChoice($decision);
+
+        return $this->serialize($choice, 'json');
     }
 }
