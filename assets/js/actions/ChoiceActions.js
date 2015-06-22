7b9c07c75f8f3fe1bf7369a16ba77c11948ff3d6
Jeff Way <jway@repx.me>
2015-06-22 00:52:44 -0700

    Move ajax calls into actions

--- assets/js/actions/ChoiceActions.js
+++ assets/js/actions/ChoiceActions.js
@@ -1,11 +1,64 @@
 import AppDispatcher from '../dispatchers/AppDispatcher.js';
 import AppConstants from '../constants/AppConstants.js';
 
-export default {
+var ChoiceActions = {
     update: (choice) => {
         AppDispatcher.dispatch({
-            type: AppConstants.UPDATE_CHOICE,
+            type: AppConstants.CHOICE.UPDATE,
             choice: choice
         });
+        ChoiceActions._updateApi(choice);
+    },
+    _updateApi: _.debounce((choice) => {
+        if (choice.name.length != 0) {
+            $.ajax({
+                accepts: "json",
+                dataType: "json",
+                method: "PUT",
+                data: choice,
+                url: Routing.generate('choice_update', {'_format': 'json', id: choice.id}),
+                success: function(data) {
+                    console.log('Choice updated', data);
+                }
+            });
+        }
+    }, 800),
+    add: _.throttle((decision_id) => {
+        $.ajax({
+            dataType: "json",
+            method: "GET",
+            url: Routing.generate('choice_new', {
+                'decision_id': decision_id,
+                '_format': 'json'
+            }),
+            success: (choice) => {
+                choice.added = true;
+                AppDispatcher.dispatch({
+                    type: AppConstants.CHOICE.ADD,
+                    choice: choice
+                });
+            }
+        })
+    }, 2000, {trailing: false}),
+    remove: (choice) => {
+        $.ajax({
+            dataType: "json",
+            method: "DELETE",
+            data: choice,
+            url: Routing.generate('choice_delete', {
+                id: choice.id,
+                '_format': 'json'
+            }),
+            success: function(data) {
+                if (data.id) {
+                    AppDispatcher.dispatch({
+                        type: AppConstants.CHOICE.REMOVE,
+                        id: data.id
+                    });
+                }
+            }
+        });
     }
-}
+};
+
+export default ChoiceActions;
--- assets/js/actions/DecisionActions.js
+++ assets/js/actions/DecisionActions.js
@@ -1,29 +1,84 @@
 import AppDispatcher from '../dispatchers/AppDispatcher.js';
 import AppConstants from '../constants/AppConstants.js';
 
-export default {
+var DecisionActions = {
     receiveMultiple: (decisions) => {
         AppDispatcher.dispatch({
-            type: AppConstants.RECEIVE_DECISIONS,
+            type: AppConstants.DECISION.RECEIVE_MULTIPLE,
             decisions: decisions
+        });
+    },
+    refresh: () => {
+        $.ajax({
+            accepts: "json",
+            dataType: "json",
+            url: Routing.generate('decisions_list', {'_format': 'json'}),
+            success: (data) => {
+                DecisionActions.receiveMultiple(data);
+            },
+            error: () => {
+                console.log('error');
+            }
         });
     },
     update: (decision) => {
         AppDispatcher.dispatch({
-            type: AppConstants.UPDATE_DECISION,
+            type: AppConstants.DECISION.UPDATE,
             decision: decision
         });
+        DecisionActions._updateApi(decision);
     },
-    add: (decision) => {
-        AppDispatcher.dispatch({
-            type: AppConstants.ADD_DECISION,
-            decision: decision
-        });
-    },
-    remove: (id) => {
-        AppDispatcher.dispatch({
-            type: AppConstants.REMOVE_DECISION,
-            id: id
+    _updateApi: _.debounce((decision) => {
+        if (decision.question.length != 0) {
+            $.ajax({
+                accepts: "json",
+                dataType: "json",
+                method: "PUT",
+                data: decision,
+                url: Routing.generate('decision_update', {
+                    '_format': 'json', id: decision.id
+                }),
+                success: (data) => {
+                    console.log('Decision updated', data);
+                }
+            });
+        }
+    }, 800),
+    add: _.throttle(() => {
+        $.ajax({
+            dataType: "json",
+            method: "GET",
+            url: Routing.generate('decision_new', {
+                '_format': 'json'
+            }),
+            success: (decision) => {
+                decision.added = true;
+                AppDispatcher.dispatch({
+                    type: AppConstants.DECISION.ADD,
+                    decision: decision
+                });
+            }
+        })
+    }, 2000, {trailing: false}),
+    remove: (decision) => {
+        $.ajax({
+            dataType: "json",
+            method: "DELETE",
+            data: decision,
+            url: Routing.generate('decision_delete', {
+                id: decision.id,
+                '_format': 'json'
+            }),
+            success: function(data) {
+                if (data.id) {
+                    AppDispatcher.dispatch({
+                        type: AppConstants.DECISION.REMOVE,
+                        id: data.id
+                    });
+                }
+            }
         });
     }
-}
+};
+
+export default DecisionActions;
